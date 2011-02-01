72a13f52bc022a169a5a94955b45d3238b47fc8c
Ryan Alyn Porter <rap@endymion.com>
2011-02-01 13:28:26 -0500

    Initial commit to wallvideo-org.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WallvideoOrg::Application.config.secret_token = '06dca1059abf69eb0e43142246703d4f4d783fdba1139504582c747cbb45ec03f3536e46eb56c9c6919f741dfa2e7665016bdd511e9c43186ed7c1745ec7cd32'
